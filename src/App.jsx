4c3b949592d503061c9b00d2a9fb31a59925de00
vuvansy <vuvansy3001@gmail.com>
2024-11-08 01:38:01 +0700

    #41. Render với điều kiện

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,13 @@ const App = () => {
         <div className="todo-container">
             <div className="todo-title">Todo List</div>
             <TodoNew addNewTodo={addNewTodo} />
-            <TodoData todoList={todoList} />
-            <div className="todo-image">
-                <img src={reactLogo} className="logo" />
-            </div>
+            {todoList.length > 0 ? (
+                <TodoData todoList={todoList} />
+            ) : (
+                <div className="todo-image">
+                    <img src={reactLogo} className="logo" />
+                </div>
+            )}
         </div>
     );
 };
