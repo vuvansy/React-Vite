94537cc91485c8e3a5c6a1d8d63ae5465f93033e
vuvansy <vuvansy3001@gmail.com>
2024-11-07 22:59:50 +0700

    #30. JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,26 @@
-//Lưu ý: tên component bắt buộc viết hoa chữ cái đầu tiên
-//Component = HTML + CSS + Javascript
-//Sử dụng Component như là 1 thẻ tag của HTML
+import "./style.css";
+/**
+ * 
+ JSX cho phép bạn code HTML trong file Javascript
+ 1.Single Root
+ JSX chỉ có 1 cha duy nhất
+ 2. Fragment
+ Fragment (mảnh vỡ) giúp bạn viết code ngắn đi, và không render “thừa html”
+ 3. Sử dụng CSS
+ Lưu ý: className
+ Không dùng từ “class” với JSX, vì class là  keyword “lớp” của javascript
+ Lưu ý về inline style (viết theo quy tắc của object)
+ */
 
 const MyComponent = () => {
-    return <div>Test Component</div>; //Tag
+    return (
+        <>
+            <div>My Component</div>
+            <div className="child" style={{ borderRadius: "10px" }}>
+                child
+            </div>
+        </>
+    );
 };
 
 export default MyComponent;
