7b9a12ac10a7cd4a727d7125149ec19d26ee59e1
vuvansy <vuvansy3001@gmail.com>
2024-11-08 17:13:26 +0700

    #71. Xử Lý Lỗi với Interceptor

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -12,8 +12,13 @@ const UserForm = () => {
         const res = await createUserAPI(fullName, email, password, phone);
         if (res.data) {
             notification.success({
-                message: "create user",
+                message: "Create user",
                 description: "Tạo user thành công",
+            });
+        } else {
+            notification.error({
+                message: "Error create user",
+                description: JSON.stringify(res.message),
             });
         }
     };
