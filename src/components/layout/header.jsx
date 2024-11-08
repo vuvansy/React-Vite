f6e6c1649673a50277a4697b57a506070350b9c2
vuvansy <vuvansy3001@gmail.com>
2024-11-08 12:48:15 +0700

    #49. Client Route với Link

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,13 +1,22 @@
-import './header.css';
+import { Link } from "react-router-dom";
+import "./header.css";
 
 const Header = () => {
     return (
         <ul>
-            <li><a className="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/products">Products</a></li>
+            <li>
+                <Link className="active" to="/">
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link to="/users">Users</Link>
+            </li>
+            <li>
+                <Link to="/products">Products</Link>
+            </li>
         </ul>
-    )
-}
+    );
+};
 
 export default Header;
