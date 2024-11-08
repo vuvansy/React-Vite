08a2e853fdadc26bf809026684783fe974404c87
vuvansy <vuvansy3001@gmail.com>
2024-11-08 16:20:08 +0700

    #69. Tạo mới User

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,5 +1,6 @@
 import { Button, Input } from "antd";
 import { useState } from "react";
+import axios from "axios";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -8,7 +9,15 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
 
     const handleClickBtn = () => {
-        console.log(">>> check state: ", { fullName, email, password, phone });
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone,
+        };
+        axios.post(URL_BACKEND, data);
+        // console.log(">>> check state: ", { fullName, email, password, phone });
     };
 
     return (
