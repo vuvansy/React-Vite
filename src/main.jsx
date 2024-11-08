19ed7befcb2a53f59abd7d0ca6a3c2b04b51b37a
vuvansy <vuvansy3001@gmail.com>
2024-11-08 12:29:20 +0700

    #46. Cấu trúc dự án React (Extra)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import UserPage from "./pages/user.jsx";
+import ProductPage from "./pages/product.jsx";
 
 const router = createBrowserRouter([
     {
@@ -10,19 +14,19 @@ const router = createBrowserRouter([
     },
     {
         path: "/login",
-        element: <div>Login Page</div>,
+        element: <LoginPage />,
     },
     {
         path: "/register",
-        element: <div>register page</div>,
+        element: <RegisterPage />,
     },
     {
         path: "/users",
-        element: <div>users page</div>,
+        element: <UserPage />,
     },
     {
         path: "/products",
-        element: <div>products page</div>,
+        element: <ProductPage />,
     },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ src/pages/product.jsx
@@ -0,0 +1,5 @@
+const ProductPage = () => {
+    return <div>Product page</div>;
+};
+
+export default ProductPage;
