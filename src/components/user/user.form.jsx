eb00c968115ddea8c636b9e8bc28ba2fc7823b8d
vuvansy <vuvansy3001@gmail.com>
2024-11-08 16:06:39 +0700

    #67. State Hóa Form

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,31 +1,61 @@
 import { Button, Input } from "antd";
+import { useState } from "react";
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleClickBtn = () => {
+        console.log(">>> check state: ", { fullName, email, password, phone });
+    };
+
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
-            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+            <div
+                style={{
+                    display: "flex",
+                    gap: "15px",
+                    flexDirection: "column",
+                }}
+            >
                 <div>
                     <span>Full Name</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => setFullName(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input />
+                    <Input
+                        value={phone}
+                        onChange={(event) => setPhone(event.target.value)}
+                    />
                 </div>
                 <div>
-                    <Button type="primary"> Create User </Button>
+                    <Button type="primary" onClick={handleClickBtn}>
+                        Create User
+                    </Button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default UserForm;
