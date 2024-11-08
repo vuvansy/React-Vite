a4933d63babf497c13a14a77481196a98e36ee97
vuvansy <vuvansy3001@gmail.com>
2024-11-08 12:45:06 +0700

    #48. Nested Routes với Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,16 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        children: [
+            {
+                path: "/users",
+                element: <UserPage />,
+            },
+            {
+                path: "/products",
+                element: <ProductPage />,
+            },
+        ],
     },
     {
         path: "/login",
@@ -20,14 +30,6 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <RegisterPage />,
-    },
-    {
-        path: "/users",
-        element: <UserPage />,
-    },
-    {
-        path: "/products",
-        element: <ProductPage />,
     },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
