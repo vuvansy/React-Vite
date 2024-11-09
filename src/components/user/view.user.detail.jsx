e7a28bde8aa24dd22df8b57c61dc8bea3561745f
vuvansy <vuvansy3001@gmail.com>
2024-11-09 23:48:14 +0700

    #83. Xử Lý Sự Kiện onChange với File

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,9 +1,33 @@
 /* eslint-disable react/prop-types */
 import { Button, Drawer } from "antd";
+import { useState } from "react";
 
 const ViewUserDetail = (props) => {
     const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
     // console.log(dataDetail);
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+
+    const handleOnChangeFile = (event) => {
+        // alert("alert me!");
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(null);
+            return;
+        }
+
+        // I've kept this example simple by using the first image instead of multiple
+        const file = event.target.files[0];
+        // console.log(file);
+        if (file) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file));
+        }
+    };
+
+    console.log(">>> check file: ", preview);
+
     return (
         <>
             <Drawer
@@ -26,10 +50,20 @@ const ViewUserDetail = (props) => {
                         <p>Phone Number: {dataDetail.phone}</p>
                         <br></br>
                         <p>Avatar</p>
-                        <div>
+                        <div
+                            style={{
+                                marginTop: "10px",
+                                height: "100px",
+                                width: "150px",
+                                border: "1px solid #ccc",
+                            }}
+                        >
                             <img
-                                height={200}
-                                width={200}
+                                style={{
+                                    height: "100%",
+                                    width: "100%",
+                                    objectFit: "contain",
+                                }}
                                 src={`${
                                     import.meta.env.VITE_BACKEND_URL
                                 }/images/avatar/${dataDetail.avatar}`}
@@ -50,9 +84,33 @@ const ViewUserDetail = (props) => {
                             >
                                 Upload Avatar
                             </label>
-                            <input type="file" hidden id="btnUpload" />
+                            <input
+                                type="file"
+                                hidden
+                                id="btnUpload"
+                                // onChange={handleOnChangeFile}
+                                onChange={(event) => handleOnChangeFile(event)}
+                            />
                         </div>
-                        
+                        {preview && (
+                            <div
+                                style={{
+                                    marginTop: "10px",
+                                    height: "100px",
+                                    width: "150px",
+                                    border: "1px solid #ccc",
+                                }}
+                            >
+                                <img
+                                    style={{
+                                        height: "100%",
+                                        width: "100%",
+                                        objectFit: "contain",
+                                    }}
+                                    src={preview}
+                                />
+                            </div>
+                        )}
                     </>
                 ) : (
                     <>
