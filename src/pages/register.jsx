bba26d5268aa1e3fcbc3c11a69e9bd1837641be3
vuvansy <vuvansy3001@gmail.com>
2024-11-10 22:57:42 +0700

    #90. Hoàn thiện tính năng Register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,33 @@
-import { Button, Input, Form } from "antd";
+import { Button, Input, Form, notification } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
+
 const RegisterPage = () => {
     const [form] = Form.useForm();
-    const onFinish = (values) => {
-        console.log(">>> check values: ", values)
-    }
+    const navigate = useNavigate();
+
+    const onFinish = async (values) => {
+        //call api
+        const res = await registerUserAPI(
+            values.fullName,
+            values.email,
+            values.password,
+            values.phone
+        );
+
+        if (res.data) {
+            notification.success({
+                message: "Register user",
+                description: "Đăng ký user thành công",
+            });
+            navigate("/login");
+        } else {
+            notification.error({
+                message: "Register user error",
+                description: JSON.stringify(res.message),
+            });
+        }
+    };
 
     return (
         <Form
@@ -20,48 +44,49 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Full Name"
                     name="fullName"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your fullName!",
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Email"
                     name="email"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your email!",
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Password"
                     name="password"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!",
+                        },
+                    ]}
                 >
                     <Input.Password />
                 </Form.Item>
                 <Form.Item
                     label="Phone number"
                     name="phone"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!",
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
--- src/services/api.service.jsx
+++ src/services/api.service.jsx
@@ -56,6 +56,17 @@ const updateUserAvatarAPI = (avatar, _id, fullName, phone) => {
     return axios.put(URL_BACKEND, data);
 };
 
+const registerUserAPI = (fullName, email, password, phone) => {
+    const URL_BACKEND = "/api/v1/user/register";
+    const data = {
+        fullName: fullName,
+        email: email,
+        password: password,
+        phone: phone,
+    };
+    return axios.post(URL_BACKEND, data);
+};
+
 export {
     createUserAPI,
     updateUserAPI,
@@ -63,4 +74,5 @@ export {
     deleteUserAPI,
     handleUploadFile,
     updateUserAvatarAPI,
+    registerUserAPI,
 };
