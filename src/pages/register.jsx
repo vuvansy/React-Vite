dd1175ac6fc685bdeaf511a0cd08f09b11396bb3
vuvansy <vuvansy3001@gmail.com>
2024-11-10 22:22:11 +0700

    #89. Sử Dụng Uncontrolled Component Cho Register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,78 @@
+import { Button, Input, Form } from "antd";
 const RegisterPage = () => {
-    return <div>Register page</div>;
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log(">>> check values: ", values)
+    }
+
+    return (
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}
+        >
+            <div
+                style={{
+                    margin: "50px",
+                }}
+            >
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                    // rules={[
+                    //     {
+                    //         required: true,
+                    //         message: 'Please input your username!',
+                    //     },
+                    // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    // rules={[
+                    //     {
+                    //         required: true,
+                    //         message: 'Please input your username!',
+                    //     },
+                    // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    // rules={[
+                    //     {
+                    //         required: true,
+                    //         message: 'Please input your username!',
+                    //     },
+                    // ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                    // rules={[
+                    //     {
+                    //         required: true,
+                    //         message: 'Please input your username!',
+                    //     },
+                    // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button onClick={() => form.submit()} type="primary">
+                        Register
+                    </Button>
+                </div>
+            </div>
+        </Form>
+    );
 };
 
 export default RegisterPage;
